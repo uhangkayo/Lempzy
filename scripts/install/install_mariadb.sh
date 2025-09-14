d2b54c0e56c1fc6c3e45368ec32cacb437daf1de
uhangkayo <uhangkayo12@gmail.com>
2025-09-15 03:52:40 +0700

    Revert "Revert "feat: add interactive installation options""

--- scripts/install/install_mariadb.sh
+++ scripts/install/install_mariadb.sh
@@ -15,10 +15,14 @@ MARIADB_VERSION=${MARIADB_VERSION:-10.11}
 
 # Install MariaDB server
 install_mariadb() {
-     echo "${grn}Installing MariaDB ${MARIADB_VERSION} ...${end}"
+echo "${grn}Installing MariaDB ${MARIADB_VERSION} ...${end}"
      echo ""
      sleep 3
+apt-get update
+     apt-get install -y curl gnupg
+     curl -LsS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | bash -s -- --mariadb-server-version="mariadb-${MARIADB_VERSION}" --skip-maxscale --skip-tools
      apt-get update
+     apt-get install -y mariadb-server
      echo ""
      sleep 1
 }
